951fb4b4d1b6321cc6d545b46ef567e221ab2fc8
Mathew Norman <spam@matnorman.com>
2018-02-16 13:40:09 +1100

    Fixed title in index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <html>
-<head><title>Home - Mat's MMS Projects</title>
+<head><title>Home - Mathew's Projects</title>
 
 <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
